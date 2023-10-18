461a7659aa65cfb8b5f2112eb2932c6434cbdc4c
Tô Minh Tiến <totiendepzai@gmail.com>
2023-10-18 19:24:55 +0700

    test

--- Manager.Designer.cs
+++ Manager.Designer.cs
@@ -339,7 +339,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(573, 54);
+            this.label1.Location = new System.Drawing.Point(589, 54);
             this.label1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(181, 41);
@@ -412,13 +412,13 @@
             // cATToolStripMenuItem
             // 
             this.cATToolStripMenuItem.Name = "cATToolStripMenuItem";
-            this.cATToolStripMenuItem.Size = new System.Drawing.Size(224, 40);
+            this.cATToolStripMenuItem.Size = new System.Drawing.Size(189, 40);
             this.cATToolStripMenuItem.Text = "CÁT";
             // 
             // đÒĂNToolStripMenuItem
             // 
             this.đÒĂNToolStripMenuItem.Name = "đÒĂNToolStripMenuItem";
-            this.đÒĂNToolStripMenuItem.Size = new System.Drawing.Size(224, 40);
+            this.đÒĂNToolStripMenuItem.Size = new System.Drawing.Size(189, 40);
             this.đÒĂNToolStripMenuItem.Text = "ĐỒ ĂN";
             // 
             // gIỚITHIỆUToolStripMenuItem
